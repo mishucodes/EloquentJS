28f467ae7ed7f9f9d545de32db2dbece8c46ea6d
Manish Goyal <manishgoyal5683@gmail.com>
2025-07-28 02:26:05 +0530

    Adding ex01 from ch02

---
+++ Chapter 02 - Program Structure/ex01.js
@@ -0,0 +1,17 @@
+//This program will print a series of #HashTags:
+
+
+
+//My Solution:
+let userInput = 5;
+for (let i = 1; i <= userInput; i++)
+{
+    let row = "";
+    for (let j = 0; j < i; j++)
+        row += "#";
+    console.log(row);
+}
+
+//Author's Solution:
+for (let line = "#"; line.length < 8; line += "#")
+    console.log(line);
